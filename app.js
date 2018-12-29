5bcadbf578a3480cfea4cd4aaba895026cb21373
Ozan ERTÜRK <ozanerturk17@gmail.com>
2018-12-29 23:59:24 +0300

    initial

---
+++ app.js
@@ -0,0 +1,145 @@
+const Telegraf = require('telegraf');
+var fs = require("fs");
+const bot = new Telegraf("790594983:AAEykc0KdVcpjvJz9XVwXbHYmSe6UnlNZuk")
+
+const Timeouts = {
+    HELLO: 1000,
+    GET_READY: 2000,
+    QUESTION: 30000,
+    NEXT_QUESTION: 25000,
+    SCOREBOARD_TIMEOUT: 15000
+}
+
+var questions = [];
+var players = [];
+var chat_id = -366641537; // CHAT ID
+var isGameOn = false;
+var current_question = undefined;
+var questionNumber = -1;
+var isQuestionActive = false;
+var scoreboardHasExpired = true;
+var questionTimeout = undefined;
+
+bot.command('scoreboard', ScoreBoardCommandHandler);
+bot.use(async (ctx, next) => {
+    if (isGameOn) {
+        console.log("nextt");
+        next();
+    } else {
+        console.log("yuttumm");
+    }
+});
+bot.command("Join", PlayerCatcher);
+bot.command("join", PlayerCatcher);
+bot.hears(GameLogic);
+bot.on("error", er => { console.log(er); })
+bot.startPolling();
+
+Initiate();
+
+
+function Initiate() {
+    fs.readFile("questions.json", 'utf-8', function (err, content) {
+        if (err) {
+            console.log("Dosya okunamadı", err);
+            return;
+        }
+        questions = JSON.parse(content);
+        if (!questions.length) {
+            console.log("soru yok!!"); return;
+        }
+        StartGame();
+    });
+}
+
+function StartGame() {
+    isGameOn = true;
+    setTimeout(
+        () => {
+            SendMessage(`Herkese selam Yarış birazdan başlamak üzere. Hazır olun ve flüt yapmayın(${Timeouts.NEXT_QUESTION/1000}saniye)`);
+            SendMessage("Lütfen Yarışmaya katılmak için /join  yazın");
+            NextQuestion();
+        }
+        , Timeouts.HELLO);
+
+}
+
+function EndGame() {
+    SendMessage("Oyun bitti Puan durumu -->");
+    ScoreBoard();
+    questionNumber = -1;
+    isGameOn = false;
+    isQuestionActive = false;
+}
+function QuestionTimeout() {
+    SendMessage(`Kimse soruyu yanıtlayamadı :(:(\n Cevap "${current_question.answers[0]}" olacaktı`);
+    isQuestionActive = false;
+    NextQuestion();
+}
+function SendMessage(message) {
+    bot.telegram.sendMessage(chat_id, `▶️${message}`, { parse_mode: 'Markdown' });
+}
+function NextQuestion() {
+    questionNumber = questionNumber + 1;
+    if (questionNumber == questions.length) {
+        EndGame();
+        return;
+    }
+    SendMessage(`${questionNumber + 1}. Soru Geliyor Hazırlan!!!`);
+    setTimeout(() => {
+        current_question = questions[questionNumber];
+        SendMessage(`✨Soru ${questionNumber + 1} (${current_question.point} Puan)✨\n >>> *${current_question.question}*`);
+        isQuestionActive = true;
+        questionTimeout = setTimeout(QuestionTimeout, Timeouts.QUESTION);
+    }, Timeouts.NEXT_QUESTION);
+}
+function PlayerCatcher(ctx) {
+    var from = ctx.message.from;
+    if (!players.filter(p => p.id == from.id).length) {
+        var new_player = {
+            id: from.id,
+            first_name: from.first_name,
+            last_name: from.last_name,
+            username: from.username,
+            point: 0
+        };
+        console.log("new player added", new_player.username);
+        SendMessage(`[${new_player.first_name}](tg://user?id=${new_player.id}) yarışa katıldı !`);
+        players.push(new_player);
+    }
+};
+function GameLogic(prediction, ctx) {
+    console.log(prediction, ctx.message);
+    const index = players.findIndex(p => p.id == ctx.message.from.id);
+    if (isGameOn && isQuestionActive && index != -1) {
+        if (current_question.answers.map(l=>l.toLowerCase()).indexOf(prediction.trim().toLowerCase()) != -1) {
+            clearTimeout(questionTimeout);
+            isQuestionActive=false;
+            const index = players.findIndex(p => p.id == ctx.message.from.id);
+            players[index].point = players[index].point + current_question.point;
+            var message = `👏👏Tebrikler [${players[index].first_name}](tg://user?id=${players[index].id})  ${current_question.point} puan kazandınız!!👏👏`;
+            SendMessage(message);
+            NextQuestion();
+        }
+    }
+};
+function ScoreBoard() {
+    var sortedList = players.sort((a, b) => b.point - a.point);
+    var scoreBoardMessage = sortedList.map((row, i) => {
+        var username = `[${row.first_name}](tg://user?id=${row.id})`;
+        text = `${i + 1}-\t${username}\t_${row.point} Point_`;
+        return text;
+    }).join("\n").replace('1', '🏆');
+    SendMessage(scoreBoardMessage);
+    scoreboardHasExpired = false;
+    setTimeout(() => scoreboardHasExpired = true, Timeouts.SCOREBOARD_TIMEOUT);
+}
+function ScoreBoardCommandHandler(ctx) {
+    if (scoreboardHasExpired) {
+        ScoreBoard();
+    }
+    else {
+        SendMessage("_!!Flüt yapmayın!!_");
+    }
+}
+
